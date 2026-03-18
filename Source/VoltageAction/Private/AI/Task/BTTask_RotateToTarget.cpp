b729bbb8958d0be1eac99188833e307f910d50d4
NamelessDAIBUTSU <ashbuns828@gmail.com>
2026-03-18 16:00:23 +0900

    敵AI　攻撃前にYawだけTargetActor方向に回転するよう対応

---
+++ Source/VoltageAction/Private/AI/Task/BTTask_RotateToTarget.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BTTask_RotateToTarget.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include <Enemy/EnemyBase.h>
+#include "AIController.h"
+
+EBTNodeResult::Type UBTTask_RotateToTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (AIController == nullptr)
+		return EBTNodeResult::Failed;
+
+	AEnemyBase* Owner = Cast<AEnemyBase>(AIController->GetPawn());
+	if (Owner == nullptr)
+		return EBTNodeResult::Failed;
+
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	if (BBComp == nullptr)
+		return EBTNodeResult::Failed;
+
+	AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject("TargetActor"));
+	if(TargetActor == nullptr)
+		return EBTNodeResult::Failed;
+
+	// Yaw方向だけプレイヤーに向かせる
+	FRotator Rotation = (TargetActor->GetActorLocation() - Owner->GetActorLocation()).Rotation();
+	const FRotator& OwnerRotation = Owner->GetActorRotation();
+	Rotation.Pitch = OwnerRotation.Pitch;
+	Rotation.Roll = OwnerRotation.Roll;
+	Owner->SetActorRotation(Rotation);
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/VoltageAction/Public/AI/Task/BTTask_RotateToTarget.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RotateToTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOLTAGEACTION_API UBTTask_RotateToTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
