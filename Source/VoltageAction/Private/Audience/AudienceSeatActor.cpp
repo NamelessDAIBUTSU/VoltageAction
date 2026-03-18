8e3280813f814be9afda332c3a72ea839543516b
NamelessDAIBUTSU <ashbuns828@gmail.com>
2026-03-18 19:17:55 +0900

    アリーナレベルの仮作成

---
+++ Source/VoltageAction/Private/Audience/AudienceSeatActor.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Audience/AudienceSeatActor.h"
+
+AAudienceSeatActor::AAudienceSeatActor()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	// 床メッシュの作成
+	FloorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorMesh"));
+	FloorMesh->SetupAttachment(RootComponent);
+
+	// 階段メッシュの生成
+	FirstStairMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FirstStairMesh"));
+	FirstStairMesh->SetupAttachment(RootComponent);
+	SecondStairMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SecondStairMesh"));
+	SecondStairMesh->SetupAttachment(RootComponent);
+	ThirdStairMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ThirdStairMesh"));
+	ThirdStairMesh->SetupAttachment(RootComponent);
+}
+
+void AAudienceSeatActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAudienceSeatActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/VoltageAction/Public/Audience/AudienceSeatActor.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AudienceSeatActor.generated.h"
+
+UCLASS()
+class VOLTAGEACTION_API AAudienceSeatActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AAudienceSeatActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// 床メッシュ
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> FloorMesh;
+
+	// 観客席メッシュ
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> FirstStairMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> SecondStairMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> ThirdStairMesh;
+};
